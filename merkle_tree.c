8631611dbd86b4600916e2a3ff7f71b59a14a0a7
stefanopier <stefano.piermatteo@gmail.com>
2026-04-29 19:36:14 +0200

    fix: update SHA256 header path for correct integration

--- merkle_tree.c
+++ merkle_tree.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
-#include "../xmss_integration/xmss_submodule/sha256.h"
+#include "../xmss/core/sha256.h"
 
 // SHA256 implementation using xmss/sha256.c
 void sha256_hash(const uint8_t* left, const uint8_t* right, uint8_t* result) {
