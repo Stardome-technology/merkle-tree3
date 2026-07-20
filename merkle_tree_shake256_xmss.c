43d9938dc1066f73378ab9202d3271c2e583a61f
stefanopier <stefano.piermatteo@gmail.com>
2026-07-20 12:58:14 +0200

    fix: replace XKCP-dependent shake256 files with fips202.h-based implementation

--- merkle_tree_shake256_xmss.c
+++ merkle_tree_shake256_xmss.c
@@ -1,7 +1,7 @@
 #include "merkle_tree_shake256_backend.h"
-#include "xmss_shake256_wrapper.h"
+#include "fips202.h"
 
 void merkle_tree_shake256_digest(const uint8_t* data, size_t len, uint8_t* result)
 {
-    xmss_shake256_wrapper(data, (unsigned long long)len, result);
+    shake256(result, 32, data, (unsigned long long)len);
 }
